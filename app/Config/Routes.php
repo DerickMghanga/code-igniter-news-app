fd33228d43b66a2975098b5d1003d31a8b38e4a4
DerickMghanga <derickmghanga3329@gmail.com>
2024-12-09 17:19:10 +0300

    first commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Controllers\News;
+use CodeIgniter\Router\RouteCollection;
+use App\Controllers\Pages;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+
+$routes->get('news', [News::class, 'index']);           
+$routes->get('news/(:segment)', [News::class, 'show']);
+
+$routes->get('pages', [Pages::class, 'index']);
+$routes->get('(:segment)', [Pages::class, 'view']);
